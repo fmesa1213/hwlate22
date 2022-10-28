b41d2e1924dcb85f7ad374d8b29aff0ddda8a62e
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-10-27 22:41:28 -0400

    Update e.6.8.cpp

--- e.6.8.cpp
+++ e.6.8.cpp
@@ -1,4 +1,4 @@
-*
+/*
 Author: Frida Mesa
 Course: CSCI-135
 Instructor: Genady Maryash
@@ -8,18 +8,25 @@ I wrote a program that checks whether two arrays have the same elements in the s
 */
 
 
-bool equal(int a[],int a_size,int b[],int b_size)
-{
-if(a_size!=b_size) 
-return false;
-else
-{
-for(int i=0;i<a_size;i++)
-{
+#include<iostream>
+using namespace std;
+
+bool equals(int a[], int a_size, int b[], int b_size){
+if(a_size != b_size) 
+return false; 
+int i;
+
+for(i=0;i<a_size;i++){ 
 if(a[i]!=b[i]) 
 return false;
 }
-return true;
+return true; 
 }
-  return 0;
+
+int main()
+{
+int a[5]={5,4,3,2,1};
+int b[5]={5,4,3,2,1};
+cout<<equals(a,5,b,5);
+return 0;
 }
